8d753e23f97cbede4cafc4771bb1de2e58c2b2b7
Vladimir Pouzanov <farcaller@gmail.com>
2012-08-31 17:16:04 +0300

    Fixed paths nesting for ObjectFileRule

--- lib/motion_build/rules/object_file_rule.rb
+++ lib/motion_build/rules/object_file_rule.rb
@@ -10,6 +10,8 @@ module MotionBuild
     def relative_source
       if source.index(project.config.get(:build_dir_objects)) == 0
         source[project.config.get(:build_dir_objects).length..-1] # return dest-relative
+      elsif source.index(project.config.get(:build_dir)) == 0
+        source[project.config.get(:build_dir).length..-1] # return dest-relative
       elsif source.index(project.config.get(:source_dir)) == 0
         source[project.config.get(:source_dir).length..-1]
       else
