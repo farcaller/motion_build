14a9ea585c14e2d1787da92a0ae90cc32381471a
Vladimir Pouzanov <farcaller@gmail.com>
2012-09-04 16:24:41 +0300

    Added a very basic plist implementation (mostly grabbed from RM itself)

--- lib/motion_build/project.rb
+++ lib/motion_build/project.rb
@@ -5,7 +5,7 @@ module MotionBuild
   class Project
     # Array of source files, required to build the project
     attr_reader :name, :config
-    attr_accessor :sources, :frameworks, :builder
+    attr_accessor :sources, :frameworks, :builder, :info_plist, :identifier, :short_version, :bundle_signature
 
     def initialize(name)
       @name = name
@@ -22,6 +22,73 @@ module MotionBuild
 
       @root_rule = Rules::BuildProjectRule.new(self)
       @root_rule.action
+    end
+
+    def info_plist
+      @info_plist ||= {
+        'BuildMachineOSBuild' => `sw_vers -buildVersion`.strip,
+        'MinimumOSVersion' => config.get(:deployment_target),
+        'CFBundleDevelopmentRegion' => 'en',
+        'CFBundleName' => name,
+        'CFBundleDisplayName' => name,
+        'CFBundleExecutable' => name,
+        'CFBundleIdentifier' => identifier,
+        'CFBundleInfoDictionaryVersion' => '6.0',
+        'CFBundlePackageType' => 'APPL',
+        'CFBundleResourceSpecification' => 'ResourceRules.plist',
+        'CFBundleShortVersionString' => short_version,
+        'CFBundleSignature' => bundle_signature,
+        'CFBundleSupportedPlatforms' => ['iPhoneOS'],
+        'CFBundleVersion' => version,
+        'CFBundleIconFiles' => icons,
+        'CFBundleIcons' => {
+          'CFBundlePrimaryIcon' => {
+            'CFBundleIconFiles' => icons,
+            'UIPrerenderedIcon' => prerendered_icon,
+          }
+        },
+        'UIAppFonts' => fonts,
+        #'UIDeviceFamily' => device_family_ints.map { |x| x.to_s },
+        #'UISupportedInterfaceOrientations' => interface_orientations_consts,
+        #'UIStatusBarStyle' => status_bar_style_const,
+        #'UIBackgroundModes' => background_modes_consts,
+        'DTXcode' => '0431',
+        'DTSDKName' => 'iphoneos5.0',
+        'DTSDKBuild' => '9A334',
+        'DTPlatformName' => 'iphoneos',
+        'DTCompiler' => 'com.apple.compilers.llvm.clang.1_0',
+        'DTPlatformVersion' => '5.1',
+        'DTXcodeBuild' => '4E1019',
+        'DTPlatformBuild' => '9B176'
+      }
+    end
+
+    def identifier
+      @identifier ||= "com.yourcompany.#{name.gsub(/\s/, '')}"
+    end
+
+    def short_version
+      @short_version ||= '1.0'
+    end
+
+    def version
+      @version ||= '1.0'
+    end
+
+    def bundle_signature
+      @bundle_signature ||= '????'
+    end
+
+    def prerendered_icon
+      @prerendered_icon ||= false
+    end
+
+    def icons
+      @icons ||= []
+    end
+
+    def fonts
+      @fonts ||= []
     end
   end
 end
